631e6f4bbb99f31d57ce51031093109c220b08f8
CarlosVF22 <cevasquezf22@gmail.com>
2024-11-13 09:56:43 -0500

    Actualizar a PHP 8.3 e instalar extensiones comunes

--- install_php8_3.sh
+++ install_php8_3.sh
@@ -12,6 +12,7 @@ sudo add-apt-repository ppa:ondrej/php -y
 
 # Actualizar los repositorios
 sudo apt update -y
+sudo apt upgrade -y
 
 # Instalar PHP 8.3 y las extensiones comunes
 sudo apt install -y php8.3 php8.3-cli php8.3-fpm php8.3-mysql php8.3-xml php8.3-mbstring php8.3-curl php8.3-zip php8.3-intl php8.3-soap php8.3-bcmath php8.3-gd php8.3-opcache php8.3-readline
